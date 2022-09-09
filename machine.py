66be52b07bb9b6d2d6394eebaf3df47fd3aff7ef
notaSWE <iamnotaswe@gmail.com>
2022-09-09 16:47:23 -0700

    Added spinning animation

--- machine.py
+++ machine.py
@@ -7,8 +7,36 @@ class Machine:
         self.display_surface = pygame.display.get_surface()
         self.reel_index = 0
         self.reel_list = {}
+        self.can_toggle = True
+        self.spinning = False
 
         self.spawn_reels()
+
+    def cooldowns(self):
+        # Only lets player spin if all reels are NOT spinning
+        for reel in self.reel_list:
+            if self.reel_list[reel].reel_is_spinning:
+                self.can_toggle = False
+                self.spinning = True
+
+        if not self.can_toggle and [self.reel_list[reel].reel_is_spinning for reel in self.reel_list].count(False) == 5:
+            self.can_toggle = True
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+
+        # Checks for space key, ability to toggle spin, and balance to cover bet size
+        # if keys[pygame.K_SPACE] and self.can_toggle and self.currPlayer.balance >= self.currPlayer.bet_size:
+        if keys[pygame.K_SPACE]:
+            self.toggle_spinning()
+            self.spin_time = pygame.time.get_ticks()
+            # self.currPlayer.place_bet()
+            # self.machine_balance += self.currPlayer.bet_size
+            # self.currPlayer.last_payout = None
+
+    def draw_reels(self, delta_time):
+        for reel in self.reel_list:
+            self.reel_list[reel].animate(delta_time)
 
     def spawn_reels(self):
         if not self.reel_list:
@@ -20,7 +48,19 @@ class Machine:
             self.reel_list[self.reel_index] = Reel((x_topleft, y_topleft)) # Need to create reel class
             self.reel_index += 1
 
+    def toggle_spinning(self):
+        if self.can_toggle:
+            self.spin_time = pygame.time.get_ticks()
+            self.spinning = not self.spinning
+            self.can_toggle = False
+
+            for reel in self.reel_list:
+                self.reel_list[reel].start_spin(int(reel) * 200)
+                # self.spin_sound.play()
+
     def update(self, delta_time):
+        self.input()
+        self.draw_reels(delta_time)
         for reel in self.reel_list:
             self.reel_list[reel].symbol_list.draw(self.display_surface)
             self.reel_list[reel].symbol_list.update()
--- reel.py
+++ reel.py
@@ -20,6 +20,38 @@ class Reel:
             pos[1] += 300
             pos = tuple(pos)
 
+    def animate(self, delta_time):
+        if self.reel_is_spinning:
+            self.delay_time -= (delta_time * 1000)
+            self.spin_time -= (delta_time * 1000)
+            reel_is_stopping = False
+
+            if self.spin_time < 0:
+                reel_is_stopping = True
+
+            # Stagger reel spin start animation
+            if self.delay_time <= 0:
+
+                # Iterate through all 5 symbols in reel; truncate; add new random symbol on top of stack
+                for symbol in self.symbol_list:
+                    symbol.rect.bottom += 100
+
+                    # Correct spacing is dependent on the above addition eventually hitting 1200
+                    if symbol.rect.top == 1200:
+                        if reel_is_stopping:
+                            self.reel_is_spinning = False
+                            # self.stop_sound.play()
+
+                        symbol_idx = symbol.idx
+                        symbol.kill()
+                        # Spawn random symbol in place of the above
+                        self.symbol_list.add(Symbol(symbols[random.choice(self.shuffled_keys)], ((symbol.x_val), -300), symbol_idx))
+
+    def start_spin(self, delay_time):
+        self.delay_time = delay_time
+        self.spin_time = 1000 + delay_time
+        self.reel_is_spinning = True
+
 class Symbol(pygame.sprite.Sprite):
     def __init__(self, pathToFile, pos, idx):
         super().__init__()
